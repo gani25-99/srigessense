4e05c901c7d46a94668d98ce37dd65268b3ef6a6
ganesh <gani91105@gmail.com>
2026-07-31 22:18:14 +0530

    Remove duplicate home mapping

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -1,29 +1,14 @@
 package com.ecommerce.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.ecommerce.service.ProductService;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private ProductService productService;
-
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    @GetMapping("/home")
-    public String home(Model model) {
-
-        model.addAttribute("products", productService.getAllProducts());
-
-        return "home";
     }
 
     @GetMapping("/admin/products")
